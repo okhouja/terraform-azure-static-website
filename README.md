f5bb607321435c553b121923274c84ee86a6c8e2
omar0940 <omar0940@gmail.com>
2024-11-25 12:53:25 +0100

    add versions and providers

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# terraform-azure-static-website
+# Terraform Azure Static Website Deployment
+
+This project demonstrates how to deploy a static website on Microsoft Azure using **Terraform** and implement a CI/CD pipeline with **GitHub Actions**.
---
+++ versions.tf
@@ -0,0 +1,14 @@
+# Terraform Settings Block
+terraform {
+  required_version = ">= 1.5.0, < 2.0"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">=4.11.0" 
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = ">= 3.0"
+    }
+  }
+}
