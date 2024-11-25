05c6d45eb5ef2703e53c3d28996a63f476247422
omar0940 <omar0940@gmail.com>
2024-11-25 21:00:30 +0100

    Terraform Azure deployment workflow #3

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,3 +2,6 @@ output "primary_web_host" {
   description = "The URL of the static website Project for ReDi School"
   value       = azurerm_storage_account.storage_account.primary_web_host
 }
+output "storage_account_name" {
+  value = azurerm_storage_account.storage_account.name
+}
